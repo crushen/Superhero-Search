03fbe451fb2d561b572560d9a8f2bbe4f7b66648
Charlotte Rushen <charlotterushen@hotmail.com>
2020-08-27 15:06:31 +0100

    Styling for superheroes list

--- src/views/Superheroes.vue
+++ src/views/Superheroes.vue
@@ -4,17 +4,19 @@
       <h1 class="heading one cursive">Superheroes A-Z</h1>
     </header>
 
-    <section>
-      <ul>
+    <section class="content">
+      <ul class="collection-wrapper">
         <li
           v-for="collection in collections"
-          :key="collection.letter">
-          <h2>{{ collection.letter }}</h2>
-          <ul>
+          :key="collection.letter"
+          class="collection">
+          <h3>{{ collection.letter }}</h3>
+          <ul class="inner">
             <li
               v-for="hero in collection.superheroes"
               :key="hero.id">
-              {{ hero.name }}
+                {{ hero.name }}
+                <img class="icon" src="@/assets/icons/arrow-right.svg" alt="">
             </li>
           </ul>
         </li>
@@ -52,5 +54,49 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
 
+.collection-wrapper {
+  margin-top: 54px;
+}
+
+.collection {
+  border: 6px solid lighten($color: $background, $amount: 7%);
+  border-radius: $border-radius;
+
+  &:not(:first-of-type) {
+    margin-top: 24px;
+  }
+
+  h3 {
+    padding: 8px;
+    background: lighten($color: $background, $amount: 7%);
+  }
+
+  .inner {
+    padding: 8px;
+
+    li {
+      padding: 2px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      &:not(:first-of-type) {
+        margin-top: 8px;
+      }
+
+      &:not(:last-of-type) {
+        border-bottom: 1px solid lighten($color: $background, $amount: 7%);
+        padding: 2px 2px 10px 2px;
+      }
+
+      .icon {
+        width: 10px;
+        margin-left: 8px;
+      }
+    }
+  }
+}
 </style>
