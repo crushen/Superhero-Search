aeb66e2a1076ca97a44b92bbb6b1eb67a75e600d
Charlotte Rushen <charlotterushen@hotmail.com>
2020-09-09 16:34:27 +0100

    Using heroes.length as offeset to fetch more data

--- src/store/modules/superheroesAtoZ.js
+++ src/store/modules/superheroesAtoZ.js
@@ -4,9 +4,8 @@ import { db } from '@/db'
 export default {
   namespaced: true,
   state: {
+    heroes: [],
     collections: [],
-    limit: 10,
-    offset: 0,
     loading: true,
     error: null,
     noScroll: false,
@@ -25,10 +24,9 @@ export default {
         .catch(error => commit('setError', error))
     },
     async getMoreHeroes({commit, state}) {
-      commit('setOffset', 100)
       commit('setLoading', true)
         await Axios
-        .get(`${db.url}/characters?ts=${db.ts}&apikey=${db.key}&hash=${db.hash}&limit=${100}&offset=${state.offset}`)
+        .get(`${db.url}/characters?ts=${db.ts}&apikey=${db.key}&hash=${db.hash}&limit=${100}&offset=${state.heroes.length}`)
         .then(response => {
           commit('setLoading', false)
           commit('sortHeroes', response.data.data.results)
@@ -38,28 +36,34 @@ export default {
   },
   mutations: {
     sortHeroes(state, array) {
-      if(!array.length) {
-        state.noScroll = true
-      } else {
+      if(array.length) {
+        // if no heroes already in state - add array
+        if(!state.heroes.length) {
+          state.heroes = array
+          // if heroes already in state - concat array
+          // state.heroes.length will be used as the offset for fetching more data
+        } else {
+          state.heroes = state.heroes.concat(array)
+        }
         while(array.length) {
           // filter heroes that don't have images
           array = array.filter(state.hasNoImage)
           // remove first item from array and get first letter
           const hero = array.shift(),
                 letter = hero.name.slice(0, 1);
-          // add first object to sorted array
+          // add first object to collections array
           if(!state.collections.length) {
             state.collections.push({
               letter: letter,
               superheroes: [hero]
             })
           } else {
-            // get most recent sorted array item to compare
+            // get most recent collection array item to compare
             const pushedCollection = state.collections[state.collections.length - 1]
-            // if letters match, push to that objects collections
+            // if letters match, push to that object to the collection
             if(letter === pushedCollection.letter) {
               pushedCollection.superheroes.push(hero)
-              // if not, add new object to sorted array
+              // if not, add new object to collections array
             } else {
               state.collections.push({
                 letter: letter,
@@ -68,11 +72,14 @@ export default {
             }
           }
         }
+        // if user reaches end of results, infinite scroll disabled
+      } else {
+        state.noScroll = true
       }
     },
     clearState(state) {
+      state.heroes = []
       state.collections = []
-      state.offset = 0
     },
     setLoading(state, set) {
       state.loading = set
