27b1c76ee34f0575eded9e8b7c64a8f209d3bd4d
Charlotte Rushen <charlotterushen@hotmail.com>
2020-10-31 13:20:32 +0000

    changed px to rem

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -30,3 +30,47 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+.search-container {
+  label {
+    width: 100%;
+    text-align: center;
+  }
+
+  .search {
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+
+    .input {
+      position: relative;
+      
+
+      img {
+        width: 24px;
+        position: absolute;
+        top: 6px;
+        left: 6px;
+      }
+
+      input[type="search"] {
+        padding: 0.625rem 1rem 0.625rem 2.5rem;
+        box-sizing: border-box;
+      }
+    }
+
+    input[type="submit"] {
+      width: 100px;
+      height: 39px;
+      background: $accent;
+      color: $light-font;
+      border-radius: $border-radius;
+      margin-left: 0.5rem;
+      font-weight: 400;
+    }
+  }
+}
+</style>
